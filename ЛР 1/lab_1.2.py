bb958cd38c10246ca8671195ba8bae4986c52020
1Sorangar1 <147050800+1Sorangar1@users.noreply.github.com>
2023-10-16 00:39:25 +0300

    Лабораторная работа 1. Исправление 1.

--- ЛР 1/lab_1.2.py
+++ ЛР 1/lab_1.2.py
@@ -1,7 +1,14 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-disk_size = 1.44*1024*1024
-book_size = 100*50*25*4
+disk_size_in_Mb = 1.44
+Kbs_in_Mb = 1024
+Bs_in_Kb = 1024
+disk_size_in_Bs = disk_size_in_Mb * Kbs_in_Mb * Bs_in_Kb
+symbols_in_line = 25
+lines_in_page = 50
+pages_in_book = 100
+symbol_size_in_Bs = 4
+book_size_in_Bs = symbols_in_line * lines_in_page * pages_in_book * symbol_size_in_Bs
 
 
-print("Количество книг, помещающихся на дискету:", int(disk_size//book_size))
+print("Количество книг, помещающихся на дискету:", int(disk_size_in_Bs//book_size_in_Bs))
